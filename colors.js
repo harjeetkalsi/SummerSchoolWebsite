75adc12c66eab6b5a8f1c13cc0da94bd80ab7dc7
harjeet <harjeet.kalsi@solirius.com>
2019-06-05 14:25:31 +0100

    Initial project

---
+++ colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#f0e6f6',
+
+  primary: '#7c37ad',
+  primaryLight: '#FB8C00',
+  primaryDark: '#FB8C00',
+
+  secondary: '#ff4081',
+  secondaryLight: '#ff79b0',
+  secondaryDark: '#c60055',
+};
